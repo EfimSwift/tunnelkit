e2cf0736c21c0b09bd25b1d4d262d8cb4b7d6bdf
Efim Soroka <efimswift@host-192-168-1-105.sohonet.ua>
2025-08-08 11:16:13 +0300

    dependencies changed

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver", from: "1.9.0"),
         .package(url: "https://github.com/passepartoutvpn/openssl-apple", from: "3.2.105"),
-        .package(url: "https://github.com/EfimSwift/wireguard-apple.git", branch: "fix-build-issues")
+        .package(path: "../wireguard-apple")
     ],
     targets: [
         .target(
